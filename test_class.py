3fdc43fb05dfa8057a9273407848fdb7aa7c11f7
Stuart Brightwell <sbrightwell@iinet.net.au>
2014-10-19 14:17:51 +1100

    Update test_class.py
    Deuce scoring and non-deuce scoring appear to be working

--- test_class.py
+++ test_class.py
@@ -34,35 +34,45 @@ class tennis_score(object):
                     self.p2_set_points[self.current_set]+=1
                 #print "End of game"
                 print self.get_set_scores()
-                self.p1_game_points = 0
-                self.p2_game_points = 0
-                self.deuce = False
                 #self.current_set+=1
+                self.reset_game_scores()
                 return True
     	    else:
     	    	return False
     	else:
             # Deuce game...
-            if ((self.p1_game_points > 5) or (self.p2_game_points > 5)):
-                print "End of deuce game"
+            if ((self.p1_game_points > 4) or (self.p2_game_points > 4)):
+                #print "End of deuce game"
                 if player == 0:
                     self.p1_game_points -=1
                 else:
                     self.p2_game_points -=1
+                if player==0:
+                    self.p1_set_points[self.current_set]+=1
+                else:
+                    self.p2_set_points[self.current_set]+=1
+                print self.get_set_scores()
+                self.reset_game_scores()
                 return True
             else:
                 return False
 	    
+    def reset_game_scores(self):
+        self.p1_game_points = 0
+        self.p2_game_points = 0
+        self.deuce = False
+
+
     def check_is_deuce(self):
-    	if (((self.p1_game_points) >= 3) and (self.p2_game_points >= 3)):
-    	    print "Deuce"
-    	    self.deuce = True
-    	    return True
-    	    
+    	#if player==0:
+        if (((self.p1_game_points-1) >= 3) and (self.p2_game_points >= 3)) or ((self.p1_game_points >= 3) and ((self.p2_game_points-1) >= 3)):
+            #print "Deuce"
+            self.deuce = True
+            return True
+            
     def get_game_scores(self, player):
-        print "Player %s scored a point" % player
         score = {self.p1_name:self.game[self.p1_game_points], self.p2_name:self.game[self.p2_game_points]}
-    	return score
+        return score
 
     def get_set_scores(self):
     	a = self.sets
@@ -70,18 +80,27 @@ class tennis_score(object):
     	return score
      
     def point_scored(self, player):
+        print "Player %s scored a point" % player
         if player==0:
-            if self.check_is_deuce() and (self.p1_game_points > 3):
-                self.p2_game_points-=1
             self.p1_game_points+=1
-            a=self.get_game_scores(0)
+            if self.check_is_deuce():
+                if (self.p2_game_points > 3):
+                    self.p2_game_points-=1
+                    self.p1_game_points-=1
+                #elif ((self.p1_game_points-1) == 3) and (self.p2_game_points == 3):
+                #self.p1_game_points-=1
+            
         elif player==1:
-            if self.check_is_deuce() and (self.p2_game_points > 3):
-                self.p1_game_points-=1
             self.p2_game_points+=1
-            a=self.get_game_scores(1)
+            if self.check_is_deuce():
+                if (self.p1_game_points > 3):
+                    self.p1_game_points-=1
+                    self.p2_game_points-=1
+            #a=self.get_game_scores(1)
 
         if not self.check_end_of_game(player):
+            #print "Not end of game yet"
+            a=self.get_game_scores(player)
             print "Score is: %s" % a
 
         
@@ -95,11 +114,11 @@ print "Sets to play: %s" % a.sets_to_play(a.match_type)
 
 a.point_scored(1) #bob 15
 a.point_scored(1) #bob 30
-#a.point_scored(0) #Albert 15
-#a.point_scored(0) #Albert 30
+a.point_scored(0) #Albert 15
+a.point_scored(0) #Albert 30
 a.point_scored(1) #Bob 40
-#a.point_scored(0)
-#a.point_scored(0) #Albert 40
+a.point_scored(0)
+a.point_scored(0) #Albert 40
 a.point_scored(1) #Bob Adv
 a.point_scored(0) #Deuce
 a.point_scored(1) #Bob Adv
